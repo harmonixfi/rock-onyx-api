dbea8047fd430a53aa09474ad1f729fdd4900e47
baonguyen1904 <bao.nlq94@gmail.com>
2024-04-23 14:09:03 +0700

    fixed issue

--- src/api/api_v1/endpoints/statistics.py
+++ src/api/api_v1/endpoints/statistics.py
@@ -11,26 +11,25 @@ import schemas
 import pandas as pd
 from api.api_v1.deps import SessionDep
 from models import Vault
+from core.config import settings
 
 router = APIRouter()
+
 
 @router.get("/{vault_id}", response_model=schemas.Statistics)
 async def get_all_statistics(session: SessionDep, vault_id: str):
 
-    statement = (select(Vault)
-        .where(Vault.id == vault_id)
-    )
+    statement = select(Vault).where(Vault.id == vault_id)
     vault = session.exec(statement).first()
 
-    statement = (select(VaultPerformance)
+    statement = (
+        select(VaultPerformance)
         .where(VaultPerformance.vault_id == vault_id)
         .order_by(VaultPerformance.datetime.desc())
     )
     performances = session.exec(statement).first()
 
-    statement = (select(UserPortfolio)
-        .where(UserPortfolio.vault_id == vault_id)
-    )
+    statement = select(UserPortfolio).where(UserPortfolio.vault_id == vault_id)
     portfolios = session.exec(statement).all()
 
     pps_history = session.exec(
@@ -40,23 +39,24 @@ async def get_all_statistics(session: SessionDep, vault_id: str):
     ).first()
     last_price_per_share = pps_history.price_per_share
     statistic = schemas.Statistics(
-        name = vault.name,
-        price_per_share = last_price_per_share,
-        apy_1y = performances.apy_ytd,
-        total_value_locked = performances.total_locked_value,
-        risk_factor = performances.risk_factor,
-        unique_depositors = len(portfolios),
-        fee_structure = json.loads(performances.fee_structure),
-        vault_address = vault.contract_address,
-        manager_address = "0x20f89bA1B0Fc1e83f9aEf0a134095Cd63F7e8CC7",
-        all_time_high_per_share = performances.all_time_high_per_share,
-        total_shares = performances.total_shares,
-        sortino_ratio= performances.sortino_ratio,
-        downside_risk = performances.downside_risk,
-        earned_fee = performances.earned_fee,
-        slug=vault.slug
+        name=vault.name,
+        price_per_share=last_price_per_share,
+        apy_1y=performances.apy_ytd,
+        total_value_locked=performances.total_locked_value,
+        risk_factor=performances.risk_factor,
+        unique_depositors=len(portfolios),
+        fee_structure=json.loads(performances.fee_structure),
+        vault_address=vault.contract_address,
+        manager_address=settings.OWNER_WALLET_ADDRESS,
+        all_time_high_per_share=performances.all_time_high_per_share,
+        total_shares=performances.total_shares,
+        sortino_ratio=performances.sortino_ratio,
+        downside_risk=performances.downside_risk,
+        earned_fee=performances.earned_fee,
+        slug=vault.slug,
     )
     return statistic
+
 
 @router.get("/", response_model=schemas.Dashboard)
 async def get_dashboard_statistics(session: SessionDep):
@@ -66,7 +66,8 @@ async def get_dashboard_statistics(session: SessionDep):
     tvl_in_all_vaults = 0
     tvl_composition = {}
     for vault in vaults:
-        statement = (select(VaultPerformance)
+        statement = (
+            select(VaultPerformance)
             .where(VaultPerformance.vault_id == vault.id)
             .order_by(VaultPerformance.datetime.desc())
         )
@@ -80,27 +81,31 @@ async def get_dashboard_statistics(session: SessionDep):
         last_price_per_share = pps_history.price_per_share
 
         statistic = schemas.Vault_Dashboard(
-            name = vault.name,
-            price_per_share = last_price_per_share,
-            apy_1y = performances.apy_ytd,
-            risk_factor = performances.risk_factor,
-            total_value_locked = performances.total_locked_value,
-            vault_address = vault.contract_address,
+            name=vault.name,
+            price_per_share=last_price_per_share,
+            apy_1y=performances.apy_ytd,
+            risk_factor=performances.risk_factor,
+            total_value_locked=performances.total_locked_value,
+            vault_address=vault.contract_address,
             slug=vault.slug,
-            id=vault.id
+            id=vault.id,
         )
         tvl_in_all_vaults += performances.total_locked_value
         tvl_composition[vault.slug] = performances.total_locked_value
         data.append(statistic)
 
     for key in tvl_composition:
-        tvl_composition[key] = tvl_composition[key]/tvl_in_all_vaults
+        tvl_composition[key] = (
+            tvl_composition[key] / tvl_in_all_vaults if tvl_in_all_vaults > 0 else 0
+        )
+
     data = schemas.Dashboard(
         tvl_in_all_vaults=tvl_in_all_vaults,
         tvl_composition=tvl_composition,
-        vaults=data
+        vaults=data,
     )
     return data
+
 
 @router.get("/{vault_id}/performance")
 async def get_vault_performance(session: SessionDep, vault_id: str):
